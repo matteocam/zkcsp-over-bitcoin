24c3700501784e8c3576c33026cb6778f6f37565
Matteo Campanelli <matteo.campanelli@gmail.com>
2017-03-14 15:29:33 -0400

    Created YaoSingleExecution dir

---
+++ YaoSingleExecution/CMakeFiles/yaoSingleExecution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yaoSingleExecution.dir/YaoSEParty.cpp.o"
+  "libyaoSingleExecution.pdb"
+  "libyaoSingleExecution.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yaoSingleExecution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
