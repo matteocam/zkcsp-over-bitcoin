4ce02bc1612566fda10846155bb659b22e25bf5e
Matteo Campanelli <matteo.campanelli@gmail.com>
2017-04-30 10:21:04 -0400

    Added simple draft of client

--- ShachamWaters/main.cpp
+++ ShachamWaters/main.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
+#include <sstream>
+#include <type_traits>
+using namespace std;
 
-#include "mylib.cpp"
-#include "gadget.hpp"
+#include <libsnark/gadgetlib1/gadgets/basic_gadgets.hpp>
+#include <libsnark/common/default_types/r1cs_ppzksnark_pp.hpp>
+#include <libsnark/common/utils.hpp>
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp>
+#include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
+#include <boost/optional.hpp>
+using namespace libsnark;
 
-char *pk = nullptr;
-char *vk = nullptr;
-size_t pk_len, vk_len;
 
-/*
-void set_keypair_cb(void *h, const char *_pk, size_t _pk_len, const char *_vk, size_t _pk_len)
+
+template<typename ppT>
+bool run_r1cs_ppzksnark(const r1cs_example<Fr<ppT> > &example,
+                        const bool test_serialization)
 {
-	pk_len = _pk_len;
-	vk_len = _vk_len;
-	
-	pk = new char[pk_len];
-	vk = new char[vk_len];
-	
-	copy(_pk, _pk+pk_len, pk);
-	copy(_vk, _vk+vk_len, vk);
+    print_header("R1CS ppzkSNARK Generator");
+    r1cs_ppzksnark_keypair<ppT> keypair = r1cs_ppzksnark_generator<ppT>(example.constraint_system);
+    printf("\n"); print_indent(); print_mem("after generator");
+
+    print_header("Preprocess verification key");
+    r1cs_ppzksnark_processed_verification_key<ppT> pvk = r1cs_ppzksnark_verifier_process_vk<ppT>(keypair.vk);
+
+    print_header("R1CS ppzkSNARK Prover");
+    r1cs_ppzksnark_proof<ppT> proof = r1cs_ppzksnark_prover<ppT>(keypair.pk, example.primary_input, example.auxiliary_input);
+    printf("\n"); print_indent(); print_mem("after prover");
+
+    print_header("R1CS ppzkSNARK Verifier");
+    const bool ans = r1cs_ppzksnark_verifier_strong_IC<ppT>(keypair.vk, example.primary_input, proof);
+    printf("\n"); print_indent(); print_mem("after verifier");
+    printf("* The verification result is: %s\n", (ans ? "PASS" : "FAIL"));
+
+    print_header("R1CS ppzkSNARK Online Verifier");
+    const bool ans2 = r1cs_ppzksnark_online_verifier_strong_IC<ppT>(pvk, example.primary_input, proof);
+    assert(ans == ans2);
+  
+
+    return ans;
 }
-* */
 
 int main(int argc, char **argv)
 {
-	mysnark_init_public_params();
-	
-	auto keypair = gen_keypair(nullptr, nullptr);
-	
-	auto dummy_input = ModularSumInput<>(2, 2, 2, 2);
-	auto proof = gen_proof((void *) &keypair, nullptr, nullptr, dummy_input, FieldT::ONE
-	
-	// NEXT: Finish this and find out how to include terms in Z_p
-	
+	default_r1cs_ppzksnark_pp::init_public_params();
+	r1cs_example<Fr<default_r1cs_ppzksnark_pp> > example = generate_r1cs_example_with_binary_input<Fr<default_r1cs_ppzksnark_pp> >(20, 10);
 	return 0;
 }
