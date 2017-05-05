3c88cf32761770381c7e450167894cf49360dad2
Matteo Campanelli <matteo.campanelli@gmail.com>
2017-05-05 18:35:46 -0400

    Fixed template parameter - drafted other gadget class

--- ShachamWaters/gadget.hpp
+++ ShachamWaters/gadget.hpp
@@ -4,59 +4,26 @@
 
 using namespace libsnark;
 
-template<typename FieldT>
-class fair_auditing_gadget : public gadget<FieldT> {
+template<typename ppT>
+class fair_auditing_gadget : public gadget<Fr<ppT>> {
 public:
-
-    //unsigned int dimension;
-
-		/*
-    pb_variable_array<FieldT> input_as_field_elements; // R1CS input 
-    pb_variable_array<FieldT> input_as_bits; // unpacked R1CS input 
-    multipacking_gadget<FieldT> unpack_inputs; // multipacking gadget 
-
-		
-    std::vector<pb_variable_array<FieldT>> puzzle_values;
-    std::vector<pb_variable_array<FieldT>> solution_values;
-    std::vector<pb_variable_array<FieldT>> encrypted_solution;
-
-    std::vector<pb_linear_combination<FieldT>> puzzle_numbers;
-    std::vector<pb_linear_combination<FieldT>> solution_numbers;
-
-    std::vector<std::shared_ptr<sudoku_cell_gadget<FieldT>>> cells;
-
-    std::vector<std::shared_ptr<sudoku_closure_gadget<FieldT>>> closure_rows;
-    std::vector<std::shared_ptr<sudoku_closure_gadget<FieldT>>> closure_cols;
-    std::vector<std::shared_ptr<sudoku_closure_gadget<FieldT>>> closure_groups;
-
-    std::shared_ptr<digest_variable<FieldT>> seed_key;
-    std::shared_ptr<digest_variable<FieldT>> h_seed_key;
-
-    std::shared_ptr<block_variable<FieldT>> h_k_block;
-    std::shared_ptr<sha256_compression_function_gadget<FieldT>> h_k_sha;
-    std::shared_ptr<sudoku_encryption_key<FieldT>> key;
-
-    pb_variable_array<FieldT> puzzle_enforce;
-    */
-		
-		const selector_gadget<FieldT> selector_g;
-		const sw_test_gadget<FieldT> sw_test_g; // Shacham-Waters test //  XXX: Should be made parametric
-		const sha256_compression_function_gadget<FieldT> sha_g;
-		const xor2<FieldT> xor2_g;
-		
-		
-		
-		
-
+typedef Fr<ppT> FieldT;
     fair_auditing_gadget(protoboard<FieldT> &pb);
     void generate_r1cs_constraints();
-    /*
-    void generate_r1cs_witness(std::vector<bit_vector> &puzzle_values,
-                               std::vector<bit_vector> &input_solution_values,
-                               bit_vector &input_seed_key,
-                               bit_vector &hash_of_input_seed_key,
-                               std::vector<bit_vector> &input_encrypted_solution);
-                               * */
+    
+    void generate_r1cs_witness();
+                               
+};
+
+template<typename ppT>
+class check_pairing_eq_gadget : public gadget<Fr<ppT>> {
+public:
+		typedef Fr<ppT> FieldT;
+    check_pairing_eq_gadget(protoboard<FieldT> &pb);
+    void generate_r1cs_constraints();
+    
+    void generate_r1cs_witness();
+                               
 };
 
 #include "gadget.tcc"
--- ShachamWaters/gadget.tcc
+++ ShachamWaters/gadget.tcc
@@ -1,12 +1,54 @@
 
-template<typename FieldT>
-fair_auditing_gadget<FieldT>::fair_auditing_gadget(protoboard<FieldT> &pb) :
-        gadget<FieldT>(pb)
-{
+template<typename ppT>
+fair_auditing_gadget<ppT>::fair_auditing_gadget(protoboard<Fr<ppT>> &pb) :
+        gadget<Fr<ppT>>(pb)
+{ 
+	// Precomputations
+	// proof_g_A_h_precomp.reset(new G1_precomputation<ppT>());
 	
+	// .reset(new precompute_G1_gadget<ppT>(pb, *(proof.g_A_h), *proof_g_A_h_precomp, FMT(annotation_prefix, " compute_proof_g_A_h_precomp")));
+	
+	// 
+  //  kc_A_valid.allocate(pb, FMT(annotation_prefix, " kc_A_valid"));
+  //  check_kc_A_valid.reset(new check_e_equals_e_gadget<ppT>(pb, *
 }
 
-template<typename FieldT>
-void fair_auditing_gadget<FieldT>::generate_r1cs_constraints()
+template<typename ppT>
+void fair_auditing_gadget<ppT>::generate_r1cs_constraints()
+{
+	
+	// 
+}
+
+template<typename ppT>
+void fair_auditing_gadget<ppT>::generate_r1cs_witness()
+{
+}
+
+
+
+template<typename ppT>
+check_pairing_eq_gadget<ppT>::check_pairing_eq_gadget(protoboard<Fr<ppT>> &pb) :
+        gadget<Fr<ppT>>(pb)
+{ 
+	// Precomputations
+	// proof_g_A_h_precomp.reset(new G1_precomputation<ppT>());
+	
+	// .reset(new precompute_G1_gadget<ppT>(pb, *(proof.g_A_h), *proof_g_A_h_precomp, FMT(annotation_prefix, " compute_proof_g_A_h_precomp")));
+	
+	// 
+  //  kc_A_valid.allocate(pb, FMT(annotation_prefix, " kc_A_valid"));
+  //  check_kc_A_valid.reset(new check_e_equals_e_gadget<ppT>(pb, *
+}
+
+template<typename ppT>
+void check_pairing_eq_gadget<ppT>::generate_r1cs_constraints()
+{
+	
+	// 
+}
+
+template<typename ppT>
+void check_pairing_eq_gadget<ppT>::generate_r1cs_witness()
 {
 }
